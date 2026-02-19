812490f325e5da8c6e153e7fef630c09941311de
Jason-zrh <renhe.zhang@hotmail.com>
2026-02-19 21:17:27 +0800

    2-19

---
+++ Algorithm/Classic150/d6单词规律.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        unordered_map<string, char> sTop;
+        unordered_map<char, string> pTos;
+
+        size_t pos = s.find(" ");
+        
+    }
+};
