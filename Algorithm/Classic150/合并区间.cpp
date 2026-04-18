3d1a6e8abe31146b73a384bdab5a86ecfdabd888
Jason-Zrh <renhe.zhang@hotmail.com>
2026-04-18 23:48:44 +0800

    4-18

---
+++ Algorithm/Classic150/合并区间.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+
+
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(),\
+             [](vector<int> a1, vector<int> a2)
+             {
+                if(a1[0] != a2[0])
+                    return a1[0] < a2[0];
+                else
+                    return a1[1] < a2[1];
+             }\
+            );
+        vector<vector<int>> ans;
+        for (auto& p : intervals) {
+            // ans存在区间且可以合并
+            if (!ans.empty() && p[0] <= ans.back()[1])
+                ans.back()[1] = max(p[1], ans.back()[1]);
+            else
+                ans.emplace_back(p);
+        }
+        return ans;
+    }
+};
